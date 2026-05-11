0e049b75b158eb45777a8db30f55b9296edd470a
Serkan Oray <serkoray1@gmail.com>
2026-05-12 01:32:29 +0200

    feat(http): add a server request handler for global request handling (exception handling of our custom HttpExceptions, letting the router route the HttpRequest)

---
+++ src/main/java/org/sports/exercise/battle/server/ServerRequestHandler.java
@@ -0,0 +1,33 @@
+package org.sports.exercise.battle.server;
+
+import org.sports.exercise.battle.server.exceptions.BadRequestException;
+import org.sports.exercise.battle.server.exceptions.UnauthorizedException;
+import org.sports.exercise.battle.server.messages.HttpRequest;
+import org.sports.exercise.battle.server.messages.HttpResponse;
+import org.sports.exercise.battle.server.router.Router;
+
+import java.util.logging.Logger;
+
+//global request handling for
+
+public class ServerRequestHandler {
+    private final Router router;
+    private final Logger logger = Logger.getLogger(ServerRequestHandler.class.getName());
+
+    public ServerRequestHandler(Router router) {
+        this.router = router;
+    }
+
+    public HttpResponse handle(HttpRequest httpRequest){
+        try{
+            logger.info(httpRequest.method() + " " + httpRequest.path());
+            return router.route(httpRequest);
+        }catch(BadRequestException e){
+            return HttpResponse.badRequest("{\"error\":\"" + e.getMessage() + "\"}");
+        }catch(UnauthorizedException e){
+            return HttpResponse.unauthorized("{\"error\":\"" + e.getMessage() + "\"}");
+        }catch(Exception e){
+            return HttpResponse.internalServerError("{\"error\":\"" + e.getMessage() + "\"}");
+        }
+    }
+}
